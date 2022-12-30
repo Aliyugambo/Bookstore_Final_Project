59dbbe11ed08d545401a9ee7ead92b1620a9a466
Aliyugambo <gamboaliyu00@gmail.com>
2022-12-30 02:09:13 +0000

    add authentication

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 const rateLimit = require("express-rate-limit")
 const CONFIG = require("./config/config");
 const logger = require("./loging/logger");
-con
+const { requiresAuth } = require('express-openid-connect');
 // SECURITY MIDDLEWARE
-const helmet = require('helmet')
+const helmet = require('helmet');
 //Routes
 const bookRouter = require("./routes/books.routes");
-const authorRouter = require("./routes/author.routes")
+const authorRouter = require("./routes/author.routes");
 const connectToDb = require("./db/mongodb");
+const auth0Middleware = require("./auth/auth0");
 
 
 const app = express();
@@ -19,25 +20,32 @@ const app = express();
 
 connectToDb();
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth0Middleware);
+
+
 const apiLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	store: new MemoryStore(),
-})
+});
 
 // Apply the rate limiting middleware to API calls only
 app.use('/api', apiLimiter);
 
 // SECURITY MIDDLEWARE
-app.use(helmet())
+app.use(helmet());
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.use("/api/v1/books", bookRouter);
-app.use("/api/v1/authors", authorRouter);
+app.use("/api/v1/books",requiresAuth(), bookRouter);
+app.use("/api/v1/authors", requiresAuth(),authorRouter);
 
 app.get('/', function (req, res) {
   res.send('Hello World');
